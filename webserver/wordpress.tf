e18abad0f8b62734594be8f8eb6df3b5e78a3d75
nazy67 <nazykh67@gmail.com>
2021-05-19 01:41:47 -0500

    modified varibles.tf, wordpress.tf

--- webserver/wordpress.tf
+++ webserver/wordpress.tf
@@ -1,13 +1,16 @@
 resource "aws_instance" "wordpress_host" {
-  depends_on = [ aws_key_pair.generated_key.name ]
-  ami                    =  data.aws_ami.amazon_linux2.image_id 
-  instance_type          =  var.instance_type      
-  vpc_security_group_ids = [ aws_security_group.wordpress_web_sg.id ]
-  user_data = data.template_file.user_data.rendered
-  key_name = bastion_key
-  
-  tags = {
-    Name        = "${var.env}_wp"
-    Environment = var.env
-  }
+  # depends_on = [ aws_key_pair.generated_key.name ]
+  ami                    = data.aws_ami.amazon_linux2.image_id
+  instance_type          = var.instance_type
+  vpc_security_group_ids = [data.terraform_remote_state.vpc.outputs.web_sg_id]
+  subnet_id              = "data.terraform_remote_state.vpc.outputs.private_subnets_ids"
+  user_data              = data.template_file.user_data.rendered
+  key_name               = bastion_key
+
+  tags = merge(
+    local.common_tags,
+    {
+      Name = "${var.env}_wordpress"
+    }
+  )
 }
