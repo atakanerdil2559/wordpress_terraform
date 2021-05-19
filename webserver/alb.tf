212e3fb8baaa1fae071353226edd6f7c53585430
nazy67 <nazykh67@gmail.com>
2021-05-19 01:42:52 -0500

    modified alb.tf, key-pair.tf

--- webserver/alb.tf
+++ webserver/alb.tf
@@ -1,22 +1,22 @@
 # Application load balancer
 resource "aws_lb" "webserver_alb" {
-  name        = "web-alb"
-  internal           = false
-  load_balancer_type = "application"
-  security_groups    = [aws_security_group.web_alb_sg.id]
-  subnets            = data.aws_subnet_ids.default.ids  #aws_subnet.public.*.id
+  name                       = "web-alb"
+  internal                   = false
+  load_balancer_type         = "application"
+  security_groups            = [aws_security_group.web_alb_sg.id]
+  subnets                    = data.aws_subnet_ids.default.ids #aws_subnet.public.*.id
   enable_deletion_protection = true
 
   tags = {
-    Name  = "${var.env}-web_alb"
+    Name = "${var.env}-web_alb"
   }
 }
 # Target group
 resource "aws_lb_target_group" "web_tg" {
-  name     = "tf-alb-tg"
-  port     = 80
-  protocol = "HTTP"
-  vpc_id   = data.aws_vpc.default.id
+  name                          = "tf-alb-tg"
+  port                          = 80
+  protocol                      = "HTTP"
+  vpc_id                        = data.aws_vpc.default.id
   load_balancing_algorithm_type = "least_outstanding_requests"
   health_check {
     path    = "/"
@@ -34,7 +34,7 @@ resource "aws_lb_target_group_attachment" "test" {
 
 resource "aws_lb_listener" "https_listener" {
   load_balancer_arn = aws_lb.webserver_alb.arn
-  port              = "443" 
+  port              = "443"
   protocol          = "HTTPS"
   ssl_policy        = "ELBSecurityPolicy-2016-08"
   certificate_arn   = data.aws_acm_certificate.amazon_issued.arn
@@ -45,9 +45,9 @@ resource "aws_lb_listener" "https_listener" {
 }
 
 resource "aws_lb_listener" "http_listener" {
-  depends_on = [  ]
+  depends_on        = []
   load_balancer_arn = aws_lb.webserver_alb.arn
-  port              = "80" 
+  port              = "80"
   protocol          = "HTTP"
   default_action {
     type             = "forward"
--- webserver/key-pair.tf
+++ webserver/key-pair.tf
@@ -1,14 +1,20 @@
-resource "tls_private_key" "my-key" {
+variable "key_name" {
+  default = "bastion_key"
+  type    = string
+}
+
+resource "tls_private_key" "bastion_key" {
   algorithm = "RSA"
   rsa_bits  = 4096
 }
 
 resource "aws_key_pair" "generated_key" {
-  key_name   = "${var.key_name}"
-  public_key = "${tls_private_key.example.public_key_openssh}"
+  key_name   = var.key_name
+  public_key = tls_private_key.bastion_key.public_key_openssh
 }
 
-resource "aws_key_pair" "bastion-key" {
-  key_name   = "bastion-key"
-  public_key = "ssh-rsa "
-} 
+# this resource blocks generated keys and stores it in state file,
+# it is not a good practice to pass around a private key but for temporary dev environment where you 
+# don't need to pass the key is ok.
+
+# key_name      = aws_key_pair.generated_key.key_name
