4b019b1a47a1d7aa88cee62064ea55f2043d3a3c
nazy67 <nazykh67@gmail.com>
2021-05-19 02:13:40 -0500

    modified alb.tf

--- webserver/alb.tf
+++ webserver/alb.tf
@@ -1,9 +1,9 @@
 resource "aws_lb" "web_lb" {
-  name        = "${var.env}-web-lb"
-  internal           = false
-  load_balancer_type = "application"
-  security_groups    = [data.terraform_remote_state.vpc.outputs.web_lb_sg_id]
-  subnets            = data.terraform_remote_state.vpc.outputs.private_subnets_ids
+  name                       = "${var.env}-web-lb"
+  internal                   = false
+  load_balancer_type         = "application"
+  security_groups            = [data.terraform_remote_state.vpc.outputs.web_lb_sg_id]
+  subnets                    = data.terraform_remote_state.vpc.outputs.private_subnets_ids
   enable_deletion_protection = true
   tags = merge(
     local.common_tags,
@@ -14,12 +14,12 @@ resource "aws_lb" "web_lb" {
 }
 
 resource "aws_lb_target_group" "web_tg" {
-  name     = "${var.env}-tg"
-  port     = 80
-  protocol = "HTTP"
-  vpc_id   = data.terraform_remote_state.vpc.outputs.vpc_id
+  name                          = "${var.env}-tg"
+  port                          = 80
+  protocol                      = "HTTP"
+  vpc_id                        = data.terraform_remote_state.vpc.outputs.vpc_id
   load_balancing_algorithm_type = "least_outstanding_requests"
-  
+
   health_check {
     path    = "/"
     port    = 80
@@ -35,11 +35,11 @@ resource "aws_lb_target_group_attachment" "attachment" {
 
 resource "aws_lb_listener" "https_listener" {
   load_balancer_arn = aws_lb.web_lb.arn
-  port              = "443" 
+  port              = "443"
   protocol          = "HTTPS"
   ssl_policy        = "ELBSecurityPolicy-2016-08"
   certificate_arn   = data.aws_acm_certificate.amazon_issued.arn
-  
+
   default_action {
     type = "redirect"
 
@@ -52,11 +52,11 @@ resource "aws_lb_listener" "https_listener" {
 }
 
 resource "aws_lb_listener" "http_listener" {
-  depends_on = [  ]
+  depends_on        = []
   load_balancer_arn = aws_lb.web_lb.arn
-  port              = "80" 
+  port              = "80"
   protocol          = "HTTP"
-  
+
   default_action {
     type             = "forward"
     target_group_arn = aws_lb_target_group.web_tg.arn
