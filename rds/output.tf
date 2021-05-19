efbed306a73a6c5b7d6d10d74e6045121889d1d5
nazy67 <nazykh67@gmail.com>
2021-05-19 01:10:30 -0500

    modified rds.tf, output.tf

--- rds/output.tf
+++ rds/output.tf
@@ -1,4 +1,15 @@
 output "rds_id" {
-  value = aws_security_group.rds.id
+  value       = aws_db_instance.rds-db.id
   description = "id of rds database"
+}
+output "rds_password" {
+  value = random_password.password.result
+}
+
+output "rds_db_username" {
+  value = aws_db_instance.rds-db.username
+}
+
+output "rds_endpoint" {
+  value = aws_db_instance.rds-db.address
 }
--- rds/rds.tf
+++ rds/rds.tf
@@ -1,23 +1,33 @@
 resource "aws_db_instance" "rds-db" {
-  allocated_storage    = var.allocated_storage
-  storage_type         = var.storage_type
-  engine               = var.engine
-  engine_version       = var.engine_version
-  instance_class       = var.instance_class
-  identifier           = "${var.env}-my-db"
-  name                 = var.database_name
-  username             = var.username
-  password             = random_password.password.result
-  skip_final_snapshot = var.skip_snapshot
+  allocated_storage         = 10
+  storage_type              = "gp2"
+  engine                    = "mariadb"
+  engine_version            = "10.5"
+  instance_class            = var.instance_class
+  identifier                = "${var.env}-my-db"
+  name                      = "my-rds"
+  username                  = "admin"
+  password                  = random_password.password.result
+  skip_final_snapshot       = var.skip_snapshot
   final_snapshot_identifier = var.skip_snapshot == true ? null : "${var.env}-rds-snapshot"
-  vpc_security_group_ids    = [ aws_security_group.rds-sg.id ]
+  vpc_security_group_ids    = [data.terraform_remote_state.vpc.outputs.rds_sg_id]
+  publicly_accessible       = var.env == "dev" ? true : false
+  tags = merge(
+    local.common_tags,
+    {
+      Name = "${var.env}_rds"
+    }
+  )
 }
 
 resource "aws_db_subnet_group" "db-subnet-grp" {
   name        = "rds-subnet-grp"
   description = "rds subnet group"
-  subnet_ids  = [aws_subnet.private-subnet-1.id, aws_subnet.private-subnet-2.id]
-  tags {
-      Name = "${var.env}-rds-subnet grp"
-  }
+  subnet_ids  = [data.terraform_remote_state.vpc.outputs.private_subnets_ids]
+  tags = merge(
+    local.common_tags,
+    {
+      Name = "${var.env}_subnet_grp"
+    }
+  )
 }
