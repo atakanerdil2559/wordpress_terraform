f964be49d2e0b3a979b2b225622bc17401d662bf
nazy67 <nazykh67@gmail.com>
2021-05-19 16:17:20 -0500

    added bastion.tf, data_source.tf

---
+++ vpc/bastion.tf
@@ -0,0 +1,27 @@
+resource "aws_instance" "bastion_host" {
+  ami                    = data.aws_ami.amazon_linux2.image_id
+  instance_type          = var.instance_type
+  vpc_security_group_ids = [aws_security_group.bastion_sg.id]
+  key_name               = "new-key"
+  tags = merge(
+    local.common_tags,
+    {
+      Name = "${var.env}_bastion"
+    }
+  )
+}
+
+variable "key_name" {
+  default = "bastion-key"
+  type    = string
+}
+
+resource "tls_private_key" "bastion_key" {
+  algorithm = "RSA"
+  rsa_bits  = 4096
+}
+
+resource "aws_key_pair" "generated_key" {
+  key_name   = var.key_name
+  public_key = tls_private_key.bastion_key.public_key_openssh
+}
