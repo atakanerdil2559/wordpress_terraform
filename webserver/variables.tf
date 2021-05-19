f7290b13771abda1c30a7364a41b4a8919b0f79e
nazy67 <nazykh67@gmail.com>
2021-05-19 01:40:15 -0500

    added remote_state.tf, variables.tf

---
+++ webserver/variables.tf
@@ -0,0 +1,16 @@
+variable "aws_region" {
+  type        = string
+  description = "the region where resources will be provisioned"
+  default = "us-east-1"
+}
+
+variable "instance_type" {
+  type = string
+  default = "t2.micro"
+}
+
+variable "env" {
+  type        = string
+  description = "name of the environment"
+  default = "dev"
+}
