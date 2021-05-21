5dc4f94be7537ea69f01a0d5bafe32eb6e0d4aff
nazy67 <nazykh67@gmail.com>
2021-05-21 15:12:35 -0500

    modified bastion.tf, added subnet_id

--- vpc/bastion.tf
+++ vpc/bastion.tf
@@ -2,6 +2,7 @@ resource "aws_instance" "bastion_host" {
   ami                    = data.aws_ami.amazon_linux2.image_id
   instance_type          = var.instance_type
   vpc_security_group_ids = [aws_security_group.bastion_sg.id]
+  subnet_id = aws_subnet.public_subnet[1].id
   key_name               = "new-key"
   tags = merge(
     local.common_tags,
