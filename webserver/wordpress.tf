93a4b01bdba0c6d567bb3c2f30a416f05e964760
nazy67 <nazykh67@gmail.com>
2021-05-21 15:28:44 -0500

    modified wordpress.tf, added subnet_id

--- webserver/wordpress.tf
+++ webserver/wordpress.tf
@@ -2,7 +2,7 @@ resource "aws_instance" "wordpress_host" {
   ami                    = data.aws_ami.amazon_linux2.image_id
   instance_type          = var.instance_type
   vpc_security_group_ids = [data.terraform_remote_state.vpc.outputs.web_sg_id]
-  # subnet_id              = ""
+  # subnet_id = "subnet-0680b3f6358d8a9fb"
   subnet_id              = "${data.terraform_remote_state.vpc.outputs.private_subnets_ids}"
   # user_data = data.template_file.user_data.rendered
   key_name  = "bastion-key"
