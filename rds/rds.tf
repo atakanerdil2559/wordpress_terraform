8fcc06b580e0dc0fd350273ef73a0864d956ab18
nazy67 <nazykh67@gmail.com>
2021-05-21 15:26:43 -0500

    modified rds.tf, added db_subnet_group_name

--- rds/rds.tf
+++ rds/rds.tf
@@ -8,6 +8,7 @@ resource "aws_db_instance" "rds-db" {
   name                      = "my_rds"
   username                  = "admin"
   password                  = random_password.password.result
+  db_subnet_group_name      = aws_db_subnet_group.db-subnet-grp.name
   skip_final_snapshot       = var.skip_snapshot
   final_snapshot_identifier = var.skip_snapshot == true ? null : "${var.env}-rds-snapshot"
   vpc_security_group_ids    = [data.terraform_remote_state.vpc.outputs.rds_sg_id]
@@ -23,8 +24,8 @@ resource "aws_db_instance" "rds-db" {
 resource "aws_db_subnet_group" "db-subnet-grp" {
   name        = "rds-subnet-grp"
   description = "rds subnet group"
-  subnet_ids = [ "subnet-099e148bc73b73598", "subnet-064f0fe34e35db49c", "subnet-0bd4818eb5b31e1d0"]
-  # subnet_ids  = [data.terraform_remote_state.vpc.outputs.private_subnets_ids]
+  subnet_ids = [ "subnet-0c041ed67fac1cd4b", "subnet-0680b3f6358d8a9fb", "subnet-0bc9131cc484f923f"]
+  # subnet_ids  = [data.terraform_remote_state.vpc.outputs.private_subnets_ids[*]]
   tags = merge(
     local.common_tags,
     {
