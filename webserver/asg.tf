359b2b26736149740f343ee89cdc231c82180d84
nazy67 <nazykh67@gmail.com>
2021-05-25 18:36:21 -0500

    modified asg.tf, changed vpc zone identifier

--- webserver/asg.tf
+++ webserver/asg.tf
@@ -1,14 +1,13 @@
 resource "aws_autoscaling_group" "web_asg" {
-  name              = "${var.env}_web_asg"
-  max_size          = 3
-  min_size          = 1
-  desired_capacity  = 3
-  health_check_type = "ELB"
-  force_delete      = true
-  target_group_arns = [aws_lb_target_group.web_tg.arn]
-  # vpc_zone_identifier =  ["${aws_subnet.private_subnet[*].id}"]
-  vpc_zone_identifier = [
-    data.terraform_remote_state.vpc.outputs.private_subnets_one,
+  name                = "${var.env}_web_asg"
+  max_size            = 3
+  min_size            = 1
+  desired_capacity    = 3
+  health_check_type   = "ELB"
+  force_delete        = true
+  target_group_arns   = [aws_lb_target_group.web_tg.arn]
+  # vpc_zone_identifier = [data.terraform_remote_state.vpc.outputs.private_subnets_[*]]
+  vpc_zone_identifier = [data.terraform_remote_state.vpc.outputs.private_subnets_one,
     data.terraform_remote_state.vpc.outputs.private_subnets_two,
   data.terraform_remote_state.vpc.outputs.private_subnets_three]
 
