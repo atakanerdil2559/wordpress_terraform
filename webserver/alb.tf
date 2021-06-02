475760e4c20b29a8db36e0ec4859204d28ebc924
nazy67 <nazykh67@gmail.com>
2021-06-01 21:48:26 -0500

    modified alb.tf, removed aws_lb_target_group_attachment

--- webserver/alb.tf
+++ webserver/alb.tf
@@ -30,12 +30,6 @@ resource "aws_lb_target_group" "web_tg" {
   }
 }
 
-# resource "aws_lb_target_group_attachment" "attachment" {
-#   target_group_arn = aws_lb_target_group.web_tg.arn
-#   target_id        = aws_instance.wordpress_host.id
-#   port             = 80
-# }
-
 resource "aws_lb_listener" "https_listener" {
   load_balancer_arn = aws_lb.web_lb.arn
   port              = "443"
@@ -54,14 +48,6 @@ resource "aws_lb_listener" "http_listener" {
   load_balancer_arn = aws_lb.web_lb.arn
   port              = "80"
   protocol          = "HTTP"
-
-  // default_action {
-  //   type = "redirect"
-  //   redirect {
-  //     port        = "443"
-  //     protocol    = "HTTPS"
-  //     status_code = "HTTP_301"
-  //   }
 
   default_action {
     type             = "forward"
