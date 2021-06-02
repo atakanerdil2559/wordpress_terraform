77d98a3bbbfb4d8f4e5e65565df5d18bb2b92d7e
nazy67 <nazykh67@gmail.com>
2021-06-01 21:27:44 -0500

    modified alb.tf, forwarded http listener to target group

--- webserver/alb.tf
+++ webserver/alb.tf
@@ -55,18 +55,16 @@ resource "aws_lb_listener" "http_listener" {
   port              = "80"
   protocol          = "HTTP"
 
+  // default_action {
+  //   type = "redirect"
+  //   redirect {
+  //     port        = "443"
+  //     protocol    = "HTTPS"
+  //     status_code = "HTTP_301"
+  //   }
+
   default_action {
     type             = "forward"
     target_group_arn = aws_lb_target_group.web_tg.arn
-  }
-
-  # default_action {
-  #   type = "redirect"
-
-  #   redirect {
-  #     port        = "443"
-  #     protocol    = "HTTPS"
-  #     status_code = "HTTP_301"
-  #   }
-  # }
+   }
 }
