3e9587313cacf7c670645311de969a050db6e2bc
nazy67 <nazykh67@gmail.com>
2021-06-02 22:27:47 -0500

    modified output.tf, removed private_key_pem

--- webserver/output.tf
+++ webserver/output.tf
@@ -2,7 +2,3 @@ output "wordpress_lb_id" {
   value       = aws_lb.web_lb.id
   description = "id of wordpress alb"
 }
-
-# output "private_key_pem" {
-#   value = module.keypair.private_key_pem
-# }
