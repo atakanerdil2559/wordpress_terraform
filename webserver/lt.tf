f785351a8de5bbcf251096e67f397e2fc6c688e1
nazy67 <nazykh67@gmail.com>
2021-06-02 23:00:40 -0500

    modified lt.tf, changed vpc_security_group_ids

--- webserver/lt.tf
+++ webserver/lt.tf
@@ -2,7 +2,7 @@ resource "aws_launch_template" "web_template" {
   name_prefix            = "${var.env}_web_template"
   image_id               = data.aws_ami.amazon_linux2.image_id
   instance_type          = var.instance_type
-  vpc_security_group_ids = [data.terraform_remote_state.vpc.outputs.web_sg_id]
+  vpc_security_group_ids = data.terraform_remote_state.vpc.outputs.web_sg_id
 
   lifecycle {
     create_before_destroy = true
