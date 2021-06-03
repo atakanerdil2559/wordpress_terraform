33039669f21b5d11278174bf5ee806c187fd86f8
nazy67 <nazykh67@gmail.com>
2021-06-02 23:03:46 -0500

    updated rds.tf, changed subnet_ids and vpc_security_group_ids

--- rds/rds.tf
+++ rds/rds.tf
@@ -11,7 +11,7 @@ resource "aws_db_instance" "rds-db" {
   db_subnet_group_name      = aws_db_subnet_group.db-subnet-grp.name
   skip_final_snapshot       = var.skip_snapshot
   final_snapshot_identifier = var.skip_snapshot == true ? null : "${var.env}-rds-snapshot"
-  vpc_security_group_ids    = [data.terraform_remote_state.vpc.outputs.rds_sg_id]
+  vpc_security_group_ids    = data.terraform_remote_state.vpc.outputs.rds_sg_id
   publicly_accessible       = var.env == "dev" ? true : false
   tags = merge(
     local.common_tags,
@@ -24,10 +24,10 @@ resource "aws_db_instance" "rds-db" {
 resource "aws_db_subnet_group" "db-subnet-grp" {
   name        = "rds-subnet-grp"
   description = "rds subnet group"
-  # subnet_ids  = [data.terraform_remote_state.vpc.outputs.private_subnets_ids[*]]
-  subnet_ids = [data.terraform_remote_state.vpc.outputs.private_subnets_one,
-    data.terraform_remote_state.vpc.outputs.private_subnets_two,
-  data.terraform_remote_state.vpc.outputs.private_subnets_three]
+  subnet_ids  = data.terraform_remote_state.vpc.outputs.private_subnets_ids
+  # subnet_ids = [data.terraform_remote_state.vpc.outputs.private_subnets_one,
+  #   data.terraform_remote_state.vpc.outputs.private_subnets_two,
+  # data.terraform_remote_state.vpc.outputs.private_subnets_three]
   tags = merge(
     local.common_tags,
     {
