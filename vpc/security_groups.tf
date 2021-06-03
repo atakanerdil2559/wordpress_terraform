b40533ac33ece5c2ef070ece475d7752b7e2328a
nazy67 <nazykh67@gmail.com>
2021-06-03 13:12:00 -0500

    added bastion security group

--- vpc/security_groups.tf
+++ vpc/security_groups.tf
@@ -1,4 +1,41 @@
-# WordPress Host security group
+# Bastion Host Security Group
+resource "aws_security_group" "bastion_sg" {
+  name        = "${var.env}_bastion_sg"
+  description = "Allow http inbound traffic to alb"
+  vpc_id      = aws_vpc.my_vpc.id
+  tags = merge(
+    local.common_tags,
+    {
+      Name = "${var.env}_bastion_sg"
+    }
+  )
+}
+resource "aws_security_group_rule" "ssh_ingress_webserver" {
+  type                     = "ingress"
+  from_port                = 22
+  to_port                  = 22
+  protocol                 = "tcp"
+  source_security_group_id = aws_security_group.web_sg.id
+  security_group_id        = aws_security_group.bastion_sg.id
+}
+resource "aws_security_group_rule" "ssh_ingress_local_laptop" {
+  type              = "ingress"
+  from_port         = 22
+  to_port           = 22
+  protocol          = "tcp"
+  cidr_blocks       = ["108.210.198.102/32"]
+  security_group_id = aws_security_group.bastion_sg.id
+}
+resource "aws_security_group_rule" "bastion_egress" {
+  type              = "egress"
+  from_port         = 0
+  to_port           = 0
+  protocol          = "-1"
+  cidr_blocks       = ["0.0.0.0/0"]
+  security_group_id = aws_security_group.bastion_sg.id
+}
+
+# Webserver security group
 resource "aws_security_group" "web_sg" {
   name        = "${var.env}_wordpress_sg"
   description = "Allow http inbound traffic to alb"
@@ -10,14 +47,7 @@ resource "aws_security_group" "web_sg" {
     }
   )
 }
-resource "aws_security_group_rule" "web_ssh_ingress" {
-  type              = "ingress"
-  from_port         = 22
-  to_port           = 22
-  protocol          = "tcp"
-  cidr_blocks       = ["108.210.198.102/32"]
-  security_group_id = aws_security_group.web_sg.id
-}
+
 resource "aws_security_group_rule" "web_http_ingress" {
   type                     = "ingress"
   from_port                = 80
